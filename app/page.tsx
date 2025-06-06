57f16abc0aac03e76a164ef0d1f1f596c3998026
elmurod <elmurodvkh@gmail.com>
2025-06-06 16:50:45 +0500

    home page part 1 - companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,50 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
+    <main>
       <h1 className='text-2xl underline'>
-        Welcome to Converso!
+        Popular Companions
       </h1>
 
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+      <section className="home-section">
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard
+          id="2"
+          name="Neura the Brainy Explorer"
+          topic="Neural network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+
+        <CompanionCard
+          id="3"
+          name="Neura the Brainy Explorer"
+          topic="Neural network of the Brain"
+          subject="science"
+          duration={45}
+          color="#ffda6e"
+        />
+      </section>
+
+      <section className="home-section">
+        <CompanionsList />
+
+        <CTA />
+      </section>
+    </main>
   )
 }
 
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CompanionsList() {
+    return (
+        <div>CompanionsList</div>
+    )
+}
